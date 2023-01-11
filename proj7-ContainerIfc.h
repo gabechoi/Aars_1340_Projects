aff97039fd69effefce13cfd349b86dace4ac77e
gabechoi <122058382+gabechoi@users.noreply.github.com>
2023-01-10 20:26:48 -0600

    Add files via upload

---
+++ proj7-ContainerIfc.h
@@ -0,0 +1,22 @@
+
+#ifndef PROJECT7_1_PROJ7_CONTAINERIFC_H
+#define PROJECT7_1_PROJ7_CONTAINERIFC_H
+
+class BADINDEX {};
+
+template <class T>
+class ContainerIfc {
+public:
+    virtual ContainerIfc<T>& pushFront(T)  =0;
+    virtual ContainerIfc<T>& pushBack(T)   =0;
+    virtual ContainerIfc<T>& popFront(T&)  =0; // throws BADINDEX
+    virtual ContainerIfc<T>& popBack(T&)   =0; // throws BADINDEX
+    virtual int getSize()         =0;
+    virtual bool isEmpty()        =0;
+    virtual T  front()            =0; // throws BADINDEX
+    virtual T  back()             =0; // throws BADINDEX
+    virtual T&  operator [](int)  =0; // throws BADINDEX
+    virtual void erase()          =0;
+};
+
+#endif //PROJECT7_1_PROJ7_CONTAINERIFC_H
